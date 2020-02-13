906e73a305889d4e30d8fe5da176585abe2ac88a
fullstack412 <dt897867@gmail.com>
2020-02-13 14:45:04 -0500

    change our values

--- wp-content/themes/perkfixone/partials/content-ourvalues.php
+++ wp-content/themes/perkfixone/partials/content-ourvalues.php
@@ -15,25 +15,25 @@
         <hr align="left">
         <div class="title">Experience Engineering</div>
         <div class="content"><p>We are at the intersection of <br/>anthropology and technology, to <br/>provide the best experiences for our <br/>clients’ employees.</p></div>
-        <div class="img"><img width="542px" height="580px" style="border-style: solid;"></div>
+        
     </div>
     <div class="ourvalues-part part3">
         <hr align="left">
         <div class="title">Win Trust</div>
         <div class="content"><p>Use intuition and data to provide <br/>great customer service, <br/>consistently.</p></div>
-        <div class="img"><img width="481px" height="682px" style="border-style: solid;"></div>
+        
     </div>
     <div class="ourvalues-part part4">
         <hr align="left">
         <div class="title">Privacy</div>
         <div class="content"><p>Provide the highest quality <br/>service without compromising <br/>privacy or security for our <br/>customers’ data.</p></div>
-        <div class="img"><img width="454px" height="676px" style="border-style: solid;"></div>
+        
     </div>
     <div class="ourvalues-part part5">
         <hr align="left">
         <div class="title">Design</div>
         <div class="content"><p>Use relevant technology to <br/>delight customers at every <br/>interaction possible.</p></div>
-        <div class="img"><img width="571px" height="733px" style="border-style: solid;"></div>
+        
     </div>
 </div>
 
