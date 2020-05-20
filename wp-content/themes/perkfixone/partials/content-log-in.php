260b27395663a75f0ff969da8c5493f2e4577ac7
steven-perkfix <steven@perkfix.com>
2020-05-21 05:39:15 +0800

    fix the typo and add styles for dropdown menu

--- wp-content/themes/perkfixone/partials/content-log-in.php
+++ wp-content/themes/perkfixone/partials/content-log-in.php
@@ -6,7 +6,7 @@
             <div class="form">
                 <form class="login-form" method="POST" action="<?php echo site_url().'/wp-json/perkstore/v1/log_in/'; ?>">
                   <h1>Log in to your <br/>Admin account</h1>
-                  <input type="text" name="login_email" placeholder="Username (corperate email)" />
+                  <input type="text" name="login_email" placeholder="Username (Corporate Email)" />
                   <input type="password" name="login_pass" placeholder="Password" />
                   <button type="submit">Login</button>
                   <p class="message">Don't have an account?<br/> <a href="<?php echo home_url('/trial');?>">Sign up here</a></p>
